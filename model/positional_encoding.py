77ce5d9b30355afa42dbe6b1d67bd31b5bdcb90d
Trapoom Ukarapol <trapoom555@gmail.com>
2023-05-09 09:44:15 +0700

    modify tensor calc in positional encoding

--- model/positional_encoding.py
+++ model/positional_encoding.py
@@ -4,6 +4,8 @@ import math
 from torch import Tensor, nn
 import torch
 
+import matplotlib.pyplot as plt
+
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model: int, dropout: float, max_len: int):
         
@@ -12,27 +14,31 @@ class PositionalEncoding(nn.Module):
 #         Input: [batch_size, seq_len, d_model]
 #         Output: [batch_size, seq_len, d_model]
 # =============================================================================
-        
-        # d_model = embedding size
+
         # max_len (use to create the buffer)
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
         
-        # [[0], [1], ... , [5000]]
+        # [max_len, 1]
         position = torch.arange(max_len).unsqueeze(1)
         
         # equivalent to 1/(10000**(2i/d_model))
-        # exponential fn is faster than original fn
+        # exponential fn is faster than power fn
+        # [d_model/2]
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)) 
-        pe = torch.zeros(max_len, 1, d_model)
-
-        pe[:, 0, ::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
         
+        pe = torch.zeros(max_len, d_model)
+        
+        pe[:, ::2] = torch.sin(position * div_term) # [max_len, d_model/2]
+        pe[:, 1::2] = torch.cos(position * div_term) # [max_len, d_model/2]
+
+        plt.imshow(pe.detach().cpu().numpy(), cmap='magma')
+        plt.show()
         # store intermediate computation in buffer to be reused in forward path
         self.register_buffer('pe', pe) 
 
-    def forward(self, x: Tensor) -> Tensor:
-        x = x + self.pe[:x.size(0)] # plus positional encoding !
+    def forward(self, x: Tensor):
+        # plus positional encoding !
+        x = x + self.pe[:x.shape[1]] 
         return self.dropout(x)
 
